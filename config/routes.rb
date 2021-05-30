ccb05f220d5be41d374dacc083a85db653d5528e
yukisa2010 <yukisa2010@gmail.com>
2021-05-30 17:40:03 +0900

    Created users index show page including user's plans

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'users#index'
+  resources 'users', only: [:index, :show, :new, :edit, :edit, :update, :destroy]
 end
